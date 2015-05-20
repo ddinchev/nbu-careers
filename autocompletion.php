8369e6ff9a50906bb852b05fb64699159f2b2d3f
Dimitar Dinchev <dinchev.dimitar@gmail.com>
2015-05-20 23:25:50 +0300

    Minor changes.

--- autocompletion.php
+++ autocompletion.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * Yii bootstrap file.
- * Used for enhanced IDE code autocompletion.
- * Note: To avoid "Multiple Implementations" PHPStorm warning and make autocomplete faster
- * exclude or "Mark as Plain Text" vendor/yiisoft/yii2/Yii.php file
- */
-class Yii extends \yii\BaseYii
-{
-    /**
-     * @var BaseApplication|WebApplication|ConsoleApplication the application instance
-     */
-    public static $app;
-}
 
 /**
  * Class BaseApplication
